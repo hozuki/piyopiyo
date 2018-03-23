cf248f63ba0245f46668d62d764cdc8f5989028f
George Wu <uiharu@buaa.edu.cn>
2018-03-23 18:17:59 +0100

    HTTP status code classification

---
+++ src/OpenMLTD.Piyopiyo/Extensions/HttpStatusCodeExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Runtime.CompilerServices;
+
+namespace OpenMLTD.Piyopiyo.Extensions {
+    public static class HttpStatusCodeExtensions {
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsContinual(this HttpStatusCode statusCode) {
+            return (int)statusCode >= 100 && (int)statusCode <= 199;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsSuccessful(this HttpStatusCode statusCode) {
+            return (int)statusCode >= 200 && (int)statusCode <= 299;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsActionRequired(this HttpStatusCode statusCode) {
+            return (int)statusCode >= 300 && (int)statusCode <= 399;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsClientError(this HttpStatusCode statusCode) {
+            return (int)statusCode >= 400 && (int)statusCode <= 499;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsServerError(this HttpStatusCode statusCode) {
+            return (int)statusCode >= 500 && (int)statusCode <= 599;
+        }
+
+    }
+}
